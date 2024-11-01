78f4c2876dcc05331cacaa1c0d5047fb0575ed53
Artem Trifonov <artemis.levitt@gmail.com>
2024-11-01 20:03:45 +0100

    Use function with multiple return values to count future values

--- investment_calculator.go
+++ investment_calculator.go
@@ -5,8 +5,10 @@ import (
 	"math"
 )
 
+const inflationRate = 2.5
+
 func countInvestment() {
-	const inflationRate = 2.5
+
 	var investmentAmount, expectedReturnRate, years float64
 
 	fmt.Print("Enter investment amount: ")
@@ -16,8 +18,14 @@ func countInvestment() {
 	fmt.Print("Enter period years: ")
 	fmt.Scan(&years)
 
-	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
-	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	futureValue, futureRealValue := calculateFutureValues(investmentAmount, expectedReturnRate, years)
+
 	fmt.Printf("Feature value: %.2f\n", futureValue)
 	fmt.Printf("Real future value: %.2f", futureRealValue)
+}
+
+func calculateFutureValues(investmentAmount, expectedReturnRate, years float64) (float64, float64) {
+	fv := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	rfv := fv / math.Pow(1+inflationRate/100, years)
+	return fv, rfv
 }
